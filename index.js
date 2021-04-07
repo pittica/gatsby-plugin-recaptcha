18f9921f720524b445eb0d9938376548c06fc1a9
Lucio Benini <iiriya@iiriya.com>
2021-04-07 23:35:10 +0200

    Fix: Unload

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export const Loader = () => {
-  if (typeof document !== "undefined") {
-    const script = document.createElement("script")
+  if (typeof document !== 'undefined') {
+    const script = document.createElement('script');
 
-    script.src = `https://www.google.com/recaptcha/api.js?render=explicit`
-    script.id = `grecaptcha-lib`
+    script.src = `https://www.google.com/recaptcha/api.js?render=explicit`;
+    script.id = `grecaptcha-lib`;
 
-    document.body.appendChild(script)
+    document.body.appendChild(script);
   }
-}
+};
 
 export const Unloader = () => {
-  if (typeof document !== "undefined") {
-    document.body.removeChild(document.getElementById("grecaptcha-lib"))
+  if (typeof document !== 'undefined') {
+    clearInterval(check);
+
+    let script = document.getElementById('grecaptcha-lib');
+
+    if (script) {
+      document.body.removeChild(script);
+    }
 
     if (IsReady()) {
-      delete window.grecaptcha
+      delete window.grecaptcha;
+
+      script = document.querySelector('[src^="https://www.gstatic.com/recaptcha/"]');
+
+      if (script) {
+        script.parentNode.removeChild(script);
+      }
     }
   }
-}
+};
 
-export const IsReady = () => typeof window !== "undefined" && typeof window.grecaptcha !== "undefined" && typeof window.grecaptcha.execute !== "undefined"
+export const IsReady = () =>
+  typeof window !== 'undefined' &&
+  typeof window.grecaptcha !== 'undefined' &&
+  typeof window.grecaptcha.execute !== 'undefined';
 
-let check
+let check;
 
 export default class ReCaptcha extends Component {
-  constructor(props, context) {
-    super(props, context)
+  constructor(props) {
+    super(props);
 
-    this.execute = this.execute.bind(this)
+    this.execute = this.execute.bind(this);
 
     this.state = {
       ready: IsReady()
-    }
+    };
 
     if (!this.state.ready) {
-      check = setInterval(this.update.bind(this), 1000)
+      check = setInterval(this.update.bind(this), 1000);
     } else {
-      this.execute()
+      this.execute();
     }
   }
 
   componentDidUpdate(_, prevState) {
     if (this.state.ready && !prevState.ready) {
-      this.execute()
+      this.execute();
     }
   }
 
   componentWillUnmount() {
-    clearInterval(check)
-
-    Unloader()
+    Unloader();
   }
 
   execute() {
@@ -62,40 +75,32 @@ export default class ReCaptcha extends Component {
         badge: this.props.badge,
         size: this.props.size,
         theme: this.props.theme
-      })
+      });
 
-      const action = this.props.action
+      const action = this.props.action;
 
-      window.grecaptcha.execute(clientId, { action })
-        .then(token => {
-          this.verify(token)
-        })
+      window.grecaptcha.execute(clientId, { action }).then((token) => {
+        this.verify(token);
+      });
     }
   }
 
   verify(token) {
-    if (typeof this.props.callback !== "undefined") {
-      this.props.callback(token)
+    if (typeof this.props.callback !== 'undefined') {
+      this.props.callback(token);
     }
   }
 
   update() {
     if (IsReady()) {
-      this.setState(() => ({ ready: true }))
+      this.setState(() => ({ ready: true }));
 
-      clearInterval(check)
+      clearInterval(check);
     }
   }
 
   render() {
-    return this.state.ready ? (
-      <div
-        id={this.props.id}
-        data-varifycallbackname="verify"
-      />
-    ) : (
-        <div id={this.props.id} className="g-recaptcha" />
-      )
+    return this.state.ready ? <div id={this.props.id} /> : <div id={this.props.id} className="g-recaptcha" />;
   }
 }
 
@@ -106,11 +111,11 @@ ReCaptcha.propTypes = {
   callback: PropTypes.func,
   sitekey: PropTypes.string.isRequired,
   action: PropTypes.string.isRequired
-}
+};
 
 ReCaptcha.defaultProps = {
-  id: "g-recaptcha",
-  badge: "inline",
-  size: "invisible",
-  theme: "light"
-}
+  id: 'g-recaptcha',
+  badge: 'inline',
+  size: 'invisible',
+  theme: 'light'
+};
